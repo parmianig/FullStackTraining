75671dcac086b70c5102aa71dcce60de5b4a391d
Primo Giuseppe Parmiani <giuseppe.parmiani@consultant.aruba.it>
2026-05-05 09:32:28 +0200

    feat: first php lesson

---
+++ HTML-CSS/Lezioni/20260504_PHP_FIRST_LESSON/index.php
@@ -0,0 +1,10 @@
+<?php
+    $nome="Alice";
+    $cognome="Rossi";
+    echo "Ciao $nome $cognome";
+
+    echo "<br>";
+    $nome="Marta";
+    $cognome="Verdi";
+    echo "Ciao $nome $cognome";
+?>
