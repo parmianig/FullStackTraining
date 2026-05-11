3b8258242f7a0c3b0adc16fd9d601037b71a5685
Primo Giuseppe Parmiani <giuseppe.parmiani@consultant.aruba.it>
2026-05-11 17:39:17 +0200

    fix: sql code fixes after tests

--- HTML-CSS/Esercizi/esercizio_20/soluzione.sql
+++ HTML-CSS/Esercizi/esercizio_20/soluzione.sql
@@ -1,11 +1,11 @@
 -- es 1 e 2
-Create table spesa {
+Create table spesa (
     id int auto_increment primary key,
     nome varchar(255) not null,
     categoria varchar(255) not null,
     quantita int not null default 1,
     prezzo decimal(10, 2) not null
-}
+);
 
 -- es 3
 -- Nome, Categoria, Quantità, Prezzo
@@ -15,15 +15,15 @@ insert into spesa (nome, categoria, quantita, prezzo) values
 ('Pane', 'Alimentari', 1, 2.00),
 ('Uova', 'Alimentari', 12, 3.00),
 ('Mele', 'Alimentari', 6, 0.50),
-('Carne', 'Alimentari', 1, 10.00)
-('Bulloni', 'Ferramenta', 10, 3.00)
-('Viti', 'Ferramenta', 4, 4.50)
+('Carne', 'Alimentari', 1, 10.00),
+('Bulloni', 'Ferramenta', 10, 3.00),
+('Viti', 'Ferramenta', 4, 4.50);
 
 -- MOSTRA TUTTO dalla tabella prodotti.
 select * from spesa;
 
 -- MOSTRA TUTTO dalla tabella prodotti DOVE la categoria è uguale a "Alimentari".
-select * from spesa where catgoria = 'Alimentari';
+select * from spesa where categoria = 'Alimentari';
 
 -- MOSTRA TUTTO dalla tabella prodotti DOVE la categoria è uguale a "Alimentari" E il prezzo è maggiore di 2.00.
 select * from spesa where categoria = 'Alimentari' and prezzo > 2.
@@ -33,4 +33,5 @@ select * from spesa where categoria = 'Alimentari' and prezzo > 2.
 alter table spesa add scadenza date after nome;
 
 -- MODIFICA prodotti AGGIUNGI nota (tipo testo) CON VALORE PREDEFINITO "Nessuna nota".
-alter table spesa add nota varchar(255) after scadenza default 'Nessuna nota';
+alter table spesa 
+add column nota varchar(255) default 'Nessuna nota' after scadenza;
